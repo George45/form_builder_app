d7e5e1085d268b53d491f4aa8f06a45381f876cf
GV <GeorgeVinters@talktalk.net>
2023-11-21 17:02:05 +0000

    Database migrations & seeder

---
+++ config/forms.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Default Base Field Path
+	|--------------------------------------------------------------------------
+	|
+	| The base path for each form field inside of /resources/views/
+	|
+	*/
+
+	'path' => env('FORM_FIELD_BASE_TEMPLATE_PATH', 'forms.fields'),
+
+];
